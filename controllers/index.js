9dc0705ad720ef4c717989191fc90840de284bfc
Crestatic <dtnguy86@gmail.com>
2022-10-13 19:33:11 -0700

    added code to server.js for socket.io

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 // import routes
 const router = require('express').Router();
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
