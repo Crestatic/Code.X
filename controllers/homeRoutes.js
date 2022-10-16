72a272c2f59fb940eddb731e6b392b668461e00c
JimJamLee <li.james.chien@gmail.com>
2022-10-15 21:04:15 -0700

    Updated routes and login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,28 +3,12 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    // const projectData = await Project.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
-
-    // // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-    //   projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
+  if (req.session.logged_in) {
+    res.redirect('/profile');
   }
+  res.render('login', { 
+    logged_in: req.session.logged_in 
+  });
 });
 
 
