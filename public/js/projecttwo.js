600f7d9256e992d5ed010ef1cb4fb87f9282353d
ashley31836 <hiashleyyu@gmail.com>
2022-10-19 23:32:36 -0700

    added projects and languages to seeds

--- public/js/projecttwo.js
+++ public/js/projecttwo.js
@@ -2,15 +2,19 @@ const filt=document.getElementById("filt"),
       list=document.querySelector("#list tbody");
 let cols={}, trs; 
 
-const table = document.querySelector('tbody');
+const table = document.querySelector('table');
 table.classList.add('hide');
+const tableHead = document.querySelector('thead');
+
+
 
 $.ajax({
   url: "/api/languages",
   method: "GET",
   dataType: 'json',
   data: 'projects',
-  success: function twoFunction (data) {
+  success: 
+  function twoFunction (data) {
     function showFilter(data) {
       if (!data.length) return;
 
@@ -20,9 +24,9 @@ $.ajax({
       };
     
       data.forEach(f => Object.entries(menu).forEach(([k, o]) => o[f[k]] = 1));
-      filt.innerHTML = Object.entries(menu).map(([k, o]) => 'Languages<br>' +
+      filt.innerHTML = Object.entries(menu).map(([k, o]) => '<h4>Languages</h4>' +
         Object.keys(o).map(c =>
-          '<label><input type="checkbox" value="' + c + '" name="' + k + '">' + c + '</label>').join(''));
+          '<label><div class="inputContainer"><input type="checkbox" value="' + c + '" name="' + k + '"><p class="labelText">' + c + '</p></div></label>').join(''));
     
       showProjects(data);
   
@@ -30,10 +34,9 @@ $.ajax({
   function showProjects(data) {
     console.log('this is data:', data);
     console.log('this is object',Object)
-    // table.classList.remove('hide');
     list.innerHTML = data.map(f => `
-  
-  <tr><td>${f.id}.</td><td>${f.language_name}</td><td>${f.projects.map(p => p.project_name).join(", ")}</td></tr>
+  <tbody>
+  <tr><td><h5>${f.id}.</h5></td><td><h5>${f.language_name}</h5></td><td><h5>${f.projects.map(p => p.project_name).join(", ")}</h5></td></tr>
   
   `).join("");
 
@@ -47,6 +50,7 @@ filt.addEventListener("change", ev => {
 
   showProjects(subset);
   table.classList.remove('hide');
+  tableHead.classList.remove('hide');
 
 });
   showFilter(data);
